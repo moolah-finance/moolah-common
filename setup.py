5bfdc3a28e851757c9989fad1a90c815a098774f
laisee <laisee@users.noreply.github.com>
2023-04-07 15:11:09 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-setup(name='common',
-      version='1.0',
+setup(name='moolahcommon',
+      version='1.1',
       py_modules=['exch_binance','portfolio','signal','config','exch_bitfinex','mockExchange','position','util','context','exchange','order'],
       )
