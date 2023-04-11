a21247cb5fa062f985c44692265556829f61cd10
Gavin <tinwald@gmail.com>
2023-04-11 15:51:13 +0800

    clean up installer

--- dist/common-1.0/setup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
-setup(name='common',
-      version='1.0',
-      py_modules=['exch_binance','portfolio','signal','config','exch_bitfinex','mockExchange','position','util','context','exchange','order'],
-      )
--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-setup(name='moolahcommon',
-      version='1.2',
-      py_modules=['exch_binance','portfolio','signal','config','exch_bitfinex','mockExchange','position','util','context','exchange','order'],
+setup(name='moolah-common',
+      description="Moolah Common lib",
+      author="laisee",
+      author_email="villa.straylite@protonmail.com",
+      url="https://github.com/moolah-finance/moolah-common",
+      version='1.5.5',
+      packages=['moolah-common']
       )
